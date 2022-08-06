8d01bacbaa46a66b77da92e7dee2418e3bd62f31
MaxSihong <maxsihong@163.com>
2022-08-06 22:20:10 +0800

    feat(初始化): 初始化项目

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>MaxSihong</title>
+    <style>
+        .container {
+            width: 60%;
+            margin: 10% auto 0;
+            background-color: #f0f0f0;
+            padding: 2% 5%;
+            border-radius: 10px
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>欢迎来到 MaxSihong</h1>
+    <h3>网站开发中...</h3>
+</div>
+</body>
